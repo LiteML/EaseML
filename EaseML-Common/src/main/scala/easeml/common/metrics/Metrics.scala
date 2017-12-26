79daafe682f9ee592d5f7491f1e7b4b8a7ddcaad
takun <yongjun.tian@tendcloud.com>
2017-12-26 14:20:19 +0800

    metrics mq

---
+++ EaseML-Common/src/main/scala/easeml/common/metrics/Metrics.scala
@@ -0,0 +1,25 @@
+package easeml.common.metrics
+
+import org.json4s.NoTypeHints
+import org.json4s.native.Serialization
+import org.json4s.native.Serialization._
+
+/**
+  * Created by takun on 26/12/2017.
+  */
+class Metrics(val id:String,
+              val metrics:Map[String,Double]) {
+  def toJSON = Metrics.toJSON(this)
+}
+
+object Metrics {
+  private implicit val formats = Serialization.formats(NoTypeHints)
+
+  def fromJSON(json:String) = {
+    read[Metrics](json)
+  }
+
+  def toJSON(metrics:Metrics) = {
+    write(metrics)
+  }
+}
