f1a568d4236f06703177746392d2d26b61cb75eb
chris <rihan.chen@tendcloud.com>
2018-01-02 11:14:35 +0800

    AngelSubmit with Register Service

--- EaseML-angel/src/main/scala/easeml/AngelSubmit.scala
+++ EaseML-angel/src/main/scala/easeml/AngelSubmit.scala
@@ -1,16 +1,23 @@
 package easeml
 
 import java.io.BufferedReader
+
 import org.json4s._
 import org.json4s.native.JsonMethods._
 import easeml.common.queue.{MessageConsumer, MessagePublisher}
 import org.apache.commons.logging.{Log, LogFactory}
 import java.util.Properties
+
+import com.tencent.angel.conf.AngelConf
+import com.tencent.angel.ml.conf.MLConf
 import easeml.common.queue.messages.Algorithm.HyperParam
 import utils.json._
 import easeml.common.queue.messages.{Algorithm, Job, RegisterAlgorithmService}
 import utils.AngelRunJar.submit
 import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.FileSystem
+import org.apache.hadoop.mapreduce.lib.input.CombineTextInputFormat
+
 import scala.io.Source
 
 /**
@@ -25,7 +32,7 @@ object AngelSubmit{
     */
   private final val confFile:String = "config.properties"
   private final val properties = new Properties()
-  private final val reader:BufferedReader = Source.fromURL(getClass.getResource(confFile)).bufferedReader()
+  private final val reader:BufferedReader = Source.fromURL(getClass.getClassLoader.getResource(confFile)).bufferedReader()
   properties.load(reader)
   private final val numThreads:Int = properties.getProperty("threads", "1").toInt
   private final val consumeHost:String = properties.getProperty("consume_host")
@@ -40,8 +47,8 @@ object AngelSubmit{
   private final val registerPassword:String = properties.getProperty("register_password")
   private final val registerQueue:String = properties.getProperty("register_queue")
 
- private final val algorithmFile:String = "angel.properties"
-  private final val algorithmMap:Map[String,String] = Source.fromFile(algorithmFile).getLines().map{f =>
+  private final val algorithmFile:String = "angel.properties"
+  private final val algorithmMap:Map[String,String] = Source.fromURL(getClass.getClassLoader.getResource(algorithmFile)).getLines().map{f =>
     f.split("=")(0) -> f.split("=")(1)
   }.toMap[String,String]
 
@@ -65,6 +72,8 @@ object AngelSubmit{
           val jobConf = new Configuration(false)
           jobConf.set("jobId", jobId)
           jobConf.set("angel.app.submit.class", algorithmMap(algorithm))
+          // Set actionType train
+          jobConf.set(AngelConf.ANGEL_ACTION_TYPE, MLConf.ANGEL_ML_TRAIN)
           val confMap: Map[String, Any] = job.params
           confMap.foreach {
             case (key, value) =>
@@ -78,7 +87,7 @@ object AngelSubmit{
   }
 
   private def registerAlgos():Unit = {
-    val json = Source.fromFile(registerJson).getLines().mkString
+    val json = Source.fromURL(getClass.getClassLoader.getResource(registerJson)).getLines().mkString
     val algosProfile = parse(json)
     val algorithms = array(algosProfile).map {algo =>
       val name = string(algo \ "name")
